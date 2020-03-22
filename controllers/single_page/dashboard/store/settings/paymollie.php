9179a7950cb52b6da21b5952f33cb16c21d15892
Ward Haché <wardhache@gmail.com>
2020-03-22 17:09:23 +0100

    refactor - update paymollie single page

--- controllers/single_page/dashboard/store/settings/paymollie.php
+++ controllers/single_page/dashboard/store/settings/paymollie.php
@@ -4,21 +4,13 @@ namespace Concrete\Package\CommunityStoreMollie\Controller\SinglePage\Dashboard\
 use Concrete\Core\Page\Controller\DashboardPageController;
 use Concrete\Core\Routing\RedirectResponse;
 use Concrete\Package\CommunityStoreMollie\Src\Mollie\Method;
-use View;
-use Loader;
-use Database;
-use \Concrete\Package\CommunityStore\Src\CommunityStore\Payment\Method as StorePaymentMethod;
-use Config;
-use Core;
 
-class Paymollie extends DashboardPageController{
-
+class Paymollie extends DashboardPageController
+{
   public function view($status = null)
   {
     $this->set('status', $status);
-
-    $apikey = $this->includePaymentAPI();
-    $this->loadPaymentData();
+    $this->set('methods', Method::getAll());
   }
 
   public function rescan()
@@ -28,21 +20,4 @@ class Paymollie extends DashboardPageController{
     $response = new RedirectResponse('/dashboard/store/settings/paymollie/rescanned');
     $response->send();
   }
-
-  public function includePaymentAPI(){
-    $this->set('apiKey', Config::get('community_store.mollie.api_key'));
-
-    return Config::get('community_store.mollie.api_key');
-  }
-  public function loadPaymentData(){
-    $this->set('form',Core::make("helper/form"));
-    //db load current mollie methods
-    $db = Database::connection();
-
-    $molliemethods = $db->fetchAll('select * from molStoreMethods');
-    $this->set('molliemethods', $molliemethods);
-  }
-
 }
-
-?>
--- single_pages/dashboard/store/settings/paymollie.php
+++ single_pages/dashboard/store/settings/paymollie.php
@@ -6,56 +6,35 @@
     </div>
 <?php } ?>
 
-<?php
-  if(empty($apiKey)){
-    echo '<h4 style="color:red;">';
-      echo t('Please set the API Key for mollie in the settings.');
-    echo '</h4>';
-  }else{
-    ?>
-    <h4>
-      <?php
-        echo t('Current mollie payment methods:')
-      ?>
-    </h4>
-    <?php
-      if(!empty($molliemethods)){
-        echo '<table class="table table-striped">';
-        echo '<tr><th style="width: 50px;">';
-        echo '</th><th>';
-        echo t('Name');
-        echo '</th><th>';
-        echo t('Minimum');
-        echo '</th><th>';
-        echo t('Maximum');
-        echo '</th></tr>';
-        foreach($molliemethods as $pmethod){
-          echo '<tr>';
-            echo '<td>';
-              echo '<img src="'.$pmethod['pImage'].'"/>';
-            echo '</td>';
-            echo '<td>';
-              echo $pmethod['pTitle'];
-            echo '</td>';
-            echo '<td>';
-              echo $pmethod['pMinimum'];
-            echo '</td>';
-            echo '<td>';
-              echo $pmethod['pMaximum'];
-            echo '</td>';
-          echo '</tr>';
-        }
-        echo '</table>';
-      }
-    ?>
-    <form action="<?php echo $this->action('rescan'); ?>" method="post">
+<?php if (!empty($methods)) { ?>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th></th>
+        <th><?php echo t('Name'); ?></th>
+        <th><?php echo t('Minimum'); ?></th>
+        <th><?php echo t('Maximum'); ?></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($methods as $method) { ?>
+        <tr>
+          <td><img src="<?php echo $method->getImage(); ?>" /></td>
+          <td><?php echo $method->getTitle(); ?></td>
+          <td><?php echo $method->getMinimum(); ?></td>
+          <td><?php echo $method->getMaximum(); ?></td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+<?php } ?>
+
+<div class="ccm-dashboard-header-buttons">
+    <form action="<?php echo $this->action('rescan'); ?>" class="form-inline" method="post">
       <?php echo $form->submit('rescan', t('Rescan'), array('class' => 'btn btn-primary')); ?>
+
+      <a href="<?= Url::to('/dashboard/store/settings#settings-payments')?>" class="btn btn-default">
+          <i class="fa fa-gear"></i> <?= t("General Settings")?>
+      </a>
     </form>
-    <p class="small">
-      <?php
-        echo t('Login to your mollie account to enable or disable the options of payment. Afterwards, click the rescan button on this page to load the methods.');
-      ?>
-    </p>
-    <?php
-  }
-?>
+</div>
